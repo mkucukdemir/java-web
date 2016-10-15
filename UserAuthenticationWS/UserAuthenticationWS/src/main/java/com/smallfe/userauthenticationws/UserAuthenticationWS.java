d013a46029b8b1ed420f76f1e20ad06732c60450
mehmet <mehmet@mehmet-U35JC>
2016-10-15 11:47:24 +0300

    User Authentication Web Service has been committed

---
+++ UserAuthenticationWS/UserAuthenticationWS/src/main/java/com/smallfe/userauthenticationws/UserAuthenticationWS.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smallfe.userauthenticationws;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ *
+ * @author mkucukdemir
+ */
+@WebService(serviceName = "UserAuthenticationWS")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class UserAuthenticationWS {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "isAuthorized")
+    public Boolean isAuthorized(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String URL = "jdbc:oracle:thin:@localhost:1521/orcl";
+            String USER = "JWAS";
+            String PASS = "JWAS_PASS";
+            Connection connection = DriverManager.getConnection(URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT 1 RESULT FROM APP_USER WHERE USERNAME='"+username+"' AND PASSWORD_DIGEST='"+password+"'");
+            try {
+                while(resultSet.next()){
+                    int supplierID = resultSet.getInt("RESULT");
+                    connection.close();
+                    return supplierID==1;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(UserAuthenticationWS.class.getName()).log(Level.SEVERE, null, ex);
+                connection.close();
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(UserAuthenticationWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+}
